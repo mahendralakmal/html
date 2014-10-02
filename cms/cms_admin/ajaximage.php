9a620ad4a78476e3f13c6866624ef6a645990d7b
Mahendra Lakmal <mahendralakmal@gmail.com>
2014-10-02 20:48:11 +0530

    Update the imageajax.php.

--- cms/cms_admin/ajaximage.php
+++ cms/cms_admin/ajaximage.php
@@ -20,21 +20,21 @@ if ((($_FILES["file"]["type"] == "image/gif")
     echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
     echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
 
-    if (file_exists("../upload/" . $_FILES["file"]["name"]))
+    if (file_exists("./image_adder/upload/" . $_FILES["file"]["name"]))
       {
      $dum =rand(5,15);
     $_FILES["file"]["name"]= $dum.$_FILES["file"]["name"];
       move_uploaded_file($_FILES["file"]["tmp_name"],
-      "../upload/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "../upload/" . $_FILES["file"]["name"];
+      "./image_adder/upload/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "./image_adder/upload/" . $_FILES["file"]["name"];
       }
     else
       {
      
 $_FILES["file"]["name"];
       move_uploaded_file($_FILES["file"]["tmp_name"],
-      "../upload/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "../upload/" . $_FILES["file"]["name"];
+      "./image_adder/upload/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "./image_adder/upload/" . $_FILES["file"]["name"];
       }
     }
   }
