c184464126dcf3c6eb8c06db113118f6c3ba6106
Mahendra Lakmal <mahendralakmal@gmail.com>
2014-10-28 11:39:07 +0530

    potimising web site part-1.

--- email/contact.php
+++ email/contact.php
@@ -1,21 +1,18 @@
-<?php
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$message = $_POST['message'];
+<?php 
+$name = $_POST['name']; 
+$email = $_POST['email']; 
+$contactno = $_POST['contactno']; 
+$message = $_POST['message']; 
+$from = "From: enquiry@sportunleash.com"; 
+$cc = "CC: powerworldcp@gmail.com, mahendralakmal@gmail.com"; 
+$subject = "Comment / enquiry" ; 
+$body = "From: $name\n E-Mail: $email\n Contact No: $contactno\n Message:\n $message"; 
+$to = "info@sportunleash.com"; 
+$headers = $from . "\r\n" . $cc; 
 
-	$from = "From: enquiry@sportunleash.com";
-	$cc = "CC: mahendralakmal@gmail.com";
-	$subject = "Comment / enquiry" ;
+if(mail($to,$subject,$body,$headers)){ 
+	header("Location: ../index.html?success=1"); 
+}
+else{ header("Location: ../index.html?success=0"); 
 
-	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
-
-	$to = "info@sportunleash.com";
-
-	$headers = $from . "\r\n" . $cc;
-
-	if(mail($to,$subject,$body,$headers)){
-		header("Location: ../index.html?success=1");
-	}else{
-		header("Location: ../index.html?success=0");
-	}
-?> 
+} ?>
