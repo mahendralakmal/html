1253232ac71e7b3d56e28a9d5f6fe3d9fe448495
Mahendra Lakmal <mahendralakmal@gmail.com>
2014-10-28 12:58:54 +0530

    Update the later tabs to slid tab content when click on the tab.

--- static/js/site.js
+++ static/js/site.js
@@ -1,72 +1,119 @@
-$(".nav-bar li").click(function(t) {
-    $(".nav-bar li.active").removeClass("active");
-    var a = $(this);
-    a.hasClass("active") || a.addClass("active"), t.preventDefault()
-}), 
+$('.nav-bar li').click(function(e) {
+    $('.nav-bar li.active').removeClass('active');
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+        $this.addClass('active');
+    }
+    e.preventDefault();
+});
 
-$(".nav-tabs li").click(function(t) {
-    $(".nav-tabs li.active").removeClass("active");
-    var a = $(this);
-    a.hasClass("active") || a.addClass("active"), t.preventDefault()
-}), 
+    $('.nav-tabs li').click(function(e) {
+    $('.nav-tabs li.active').removeClass('active');
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+        $this.addClass('active');
+    }
+    e.preventDefault();
+});
 
-$(document).on("click", ".info_ly .close", function() {
-    $(this).parent("div").slideUp()
-}), $(document).on("click", ".slide-content #sus-cntry", function() {
-    $(this).parent("div").slideUp()
-}), 
+
+$(document).on('click', '.info_ly .close', function () {
+    $(this).parent('div').slideUp();
+});
+
+$(document).on('click', '.slide-content #sus-cntry', function () {
+    $(this).parent('div').slideUp();
+});
+
 $(".video-content").load("../includes/sport-unleash-video-section.html");
 $(".enquir").load("../includes/enquiry.html");
 $("footer").load("../includes/footer.html");
-$(document).ready(function() {
-    function t(t) {
-        return n == t ? ($(".info_ly").slideUp(), void(n = "")) : ($(".info_ly").slideUp(), $(".info_ly").html(t), $(".info_ly").addClass("jumbotron"), $(".info_ly").slideDown(), void(n = t))
+
+$(document).ready(function(){
+    var lastData;
+    /******** THIS IS FOR THE NAVIGATION ********/
+    $(".nav-bar li").click(function(e) {
+        loadData($("." + $(this).attr('rel')).html());
+        $(".info_ly").focus();        
+        e.preventDefault();
+    });
+
+    $(".callout-actions li").click(function(e) {
+        loadData($("." + $(this).attr('rel')).html());
+        $(".info_ly").focus();
+        $("html, body").animate({ scrollTop: $(".logo").offset().top}, 1000);
+        e.preventDefault();
+    });
+
+    function loadData(data) {
+        if(lastData == data) {
+            $(".info_ly").slideUp();
+            lastData = '';
+            return;
+        }
+        $(".info_ly").slideUp();
+        $(".info_ly").html(data);
+        $(".info_ly").addClass('jumbotron');
+        $(".info_ly").slideDown();
+        lastData = data;
     }
 
-    function a(t) {
-        $(".slide-content").slideUp(), 
-        $(".slide-content").html(t), 
-        $(".slide-content").addClass("jumbotron"), 
-        $(".slide-content").slideDown(), n = t
+    $('.nav-tabs li').click(function(){
+        loadTabData($("." + $(this).attr('rel')).html());
+        $(".tab_info_ly").focus();
+        $("html, body").animate({ scrollTop: $(".nav-tabs").offset().top}, 500);        
+        e.preventDefault();
+    });
+
+    function loadTabData(data) {
+        if(lastData == data) {
+            $(".tab_info_ly").slideUp();
+            lastData = '';
+            return;
+        }
+        $(".tab_info_ly").slideUp();
+        $(".tab_info_ly").html(data);
+        $(".tab_info_ly").slideDown();
+        lastData = data;
     }
-    var n;
-    $(".nav-bar li").click(function(a) {
-        t($("." + $(this).attr("rel")).html()), $(".info_ly").focus(), a.preventDefault()
-    }), 
+
+    function loadNavData(data) {
+        if(lastData == data) {
+            $(".slide-content").slideUp();
+            lastData = '';
+            return;
+        }
+        $(".slide-content").slideUp();
+        $(".slide-content").html(data);
+        $(".slide-content").addClass('jumbotron');
+        $(".slide-content").slideDown();
+        lastData = data;
+    }
+    /******** THIS IS FOR THE CONTACT A SUS BANNER TO LOAD THE COUNTRY LIST ********/
+    $(".contact-sus").click(function(e) {
+        $(".slide-content").load("../includes/country-list.html"),1000;
+        loadCountryData($("." + $(this).attr('rel')).html());
+        $("html, body").animate({ scrollTop: $(".slide-content").offset().top}, 1000);
+        $(".slide-content").focus();
+        e.preventDefault();
+    });
+
+    function loadCountryData(data) {
+        $(".slide-content").slideUp();
+        $(".slide-content").html(data);        
+        $(".slide-content").addClass('jumbotron');
+        $(".slide-content").slideDown();
+        lastData = data;
+    }
 
 
-    $(".callout-actions li").click(function(a) {
-        t($("." + $(this).attr("rel")).html()), $(".info_ly").focus(), $("html, body").animate({
-            scrollTop: $(".logo").offset().top
-        }, 1e3), a.preventDefault()
-    }), 
+    $('#dynamic_select').bind('change', function () {
+        var url = $(this).val(); // get selected value
+            if (url) { // require a URL
+                window.location = url; // redirect
+            }
+        return false;
+    });
 
-
-    $(".nav-tabs li a").click(function() {
-        $("html, body").animate({
-            scrollTop: $(".nav-tabs").offset().top
-        }, 500)
-    }), 
-
-    $(".nav-tabs li a.tab1").click(function() {
-        $(".tab-content .tab2,.tab3,.tab4").removeClass("active"), $(".tab-content .tab1").addClass("active")
-    }), 
-    $(".nav-tabs li a.tab2").click(function() {
-        $(".tab-content .tab1,.tab3,.tab4").removeClass("active"), $(".tab-content .tab2").addClass("active")
-    }), 
-    $(".nav-tabs li a.tab3").click(function() {
-        $(".tab-content .tab1,.tab2,.tab4").removeClass("active"), $(".tab-content .tab3").addClass("active")
-    }), 
-    $(".nav-tabs li a.tab4").click(function() {
-        $(".tab-content .tab1,.tab2,.tab3").removeClass("active"), $(".tab-content .tab4").addClass("active")
-    }), 
-
-    $(".contact-sus").click(function(t) {
-        $(".slide-content").load("../includes/country-list.html"), 1e3, a($("." + $(this).attr("rel")).html()), $("html, body").animate({
-            scrollTop: $(".slide-content").offset().top
-        }, 1e3), $(".slide-content").focus(), t.preventDefault()
-    }), $("#dynamic_select").bind("change", function() {
-        var t = $(this).val();
-        return t && (window.location = t), !1
-    }), $("#dynamic_select").addClass("form-control")
+    $('#dynamic_select').addClass('form-control');
 });
