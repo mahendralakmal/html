ef1604e029ad0b6219a4077f15c370281735b57f
Mahendra Lakmal <mahendralakmal@gmail.com>
2014-12-01 14:45:42 +0530

    Update with the page load pannel.

--- static/js/site.js
+++ static/js/site.js
@@ -1,3 +1,40 @@
+$(document).ready(function() {  
+ 
+    var id = '#dialog';
+         
+    /*Get the screen height and width*/
+    var maskHeight = $(document).height();
+    var maskWidth = $(window).width();
+         
+    /*Set heigth and width to mask to fill up the whole screen*/
+    $('#mask').css({'width':maskWidth,'height':maskHeight});
+     
+    /*transition effect*/
+    $('#mask').fadeIn(10); 
+    $('#mask').fadeTo("slow",0.4);  
+         
+    /*Get the window height and width*/
+    var winH = $(window).height();
+    var winW = $(window).width();
+                   
+    /*Set the popup window to center*/
+    $(id).css('top',  winH/2-$(id).height()/2);
+    $(id).css('left', winW/2-$(id).width()/2);
+         
+    /*transition effect*/
+    $(id).fadeIn(1000);      
+    $(id).fadeTo("slow",0.8);    
+         
+    /*if close button is clicked*/
+    $('.window .close').click(function (e) {
+        /*Cancel the link behavior*/
+        e.preventDefault();
+         
+        $('#mask').hide();
+        $('.window').hide();
+    });    
+});
+
 $('.nav-bar li').click(function(e) {
     $('.nav-bar li.active').removeClass('active');
     var $this = $(this);
