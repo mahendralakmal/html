13e5b51aa101bfcaed0f8ed17d1b5911738d9830
Mahendra Lakmal <mahendralakmal@gmail.com>
2014-12-08 17:03:31 +0530

    Update the admin page

--- admin/home.php
+++ admin/home.php
@@ -7,14 +7,14 @@
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="./static/dist/js/bootstrap.min.js"></script>
+	<script src="./static/js/jquery.min.js"></script>
 	<script src="./static/dist/js/site.js"></script>
 </head>
 <body>
 
 
-	<div class="container content-main ">		
+	<div class="container content-main ">
 		<?php include_once('./includes/header.php'); ?>
-		
 		<div class="wrap-logo row">
 		<?php include_once('./includes/menu.php'); ?>
 		<div class="col-md-9  content-height pnl-right"></div>
--- admin/includes/css.php
+++ admin/includes/css.php
@@ -1,9 +1,11 @@
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="./static/dist/css/bootstrap.min.css">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="./static/dist/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 
 
- <link rel="stylesheet" href="../static/css/site.css">
+<link rel="stylesheet" href="../static/css/site.css">
 <link rel="stylesheet" href="./static/css/site.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+
+
